5941b39b71790324d6340b89523bb4eaa943f946
muni <booscarface@gmail.com>
2023-09-14 08:59:40 +0200

    added github action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 
+// commit node_modules: they must be available
 function run() {
     core.notice('Hello from my custom javascript action')      
 }
