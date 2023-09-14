6225c78f8ef18323227641819c282cf0c2ae2e5c
muni <booscarface@gmail.com>
2023-09-14 10:12:15 +0200

    fixed css and paragraphs

--- src/components/HelpArea.jsx
+++ src/components/HelpArea.jsx
@@ -5,17 +5,17 @@ const HELP_ITEMS = [
   {
     id: 'h1',
     title: 'What is Git?',
-    text: 'Git is a version control system, helping you to manage your code and create code snapshots.',
+    text: 'Git is a distributed version control system that tracks changes in any set of computer files, usually used for coordinating work among programmers who are collaboratively developing source code during software development',
   },
   {
     id: 'h2',
     title: 'What is GitHub?',
-    text: 'GitHub is a company and online offering, providing you with tons of Git-related services (e.g., cloud repositories).',
+    text: 'GitHub, Inc. is a platform and cloud-based service for software development and version control using Git, allowing developers to store and manage their code.',
   },
   {
     id: 'h3',
     title: 'What is GitHub Actions?',
-    text: 'GitHub Actions is an automation service (or CI / CD service) that helps you automate repository-related workflows and processes.',
+    text: 'GitHub Actions is a continuous integration and continuous delivery (CI/CD) platform that allows you to automate your build, tet, and deployment pipeline.',
   },
 ];
 
